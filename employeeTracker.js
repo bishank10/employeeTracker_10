8b166cde4f4a5d421bf14491c27b4d20d202761c
bishank <chandbishank@gmail.com>
2020-10-28 15:42:12 -0600

    created the required functions

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,11 +1,84 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
+
 const connection = mysql.createConnection({
   host: "localhost",
-  // Your port; if not 3306
   port: 3306,
-  // Your username
   user: "root",
-  // Your password
   password: "Kathmandu@123",
   database: "employee_tracker"
 });
+
+connection.connect(function(err){
+  if(err) throw err;
+  startApp();
+})
+
+// first question 
+let questions= [
+  {
+      type : "list",
+      message : "Select from one of the following option?",
+      choices : ["add department", "add employee", "add role", "view department", "view roles", "view employees", "update employee role"],
+      name : "selected "
+
+  }
+];
+
+
+function startApp(){
+  inquirer.prompt(questions)
+  .then(res => {
+  switch(res.selected){
+    case "add department":
+      addDepartment()
+      break;
+
+      case "add employee":
+      addEmployee()
+      break;
+
+      case "add role":
+      addRole()
+      break;
+
+      case "view department":
+      viewDepartment()
+      break;
+
+      case "view roles":
+      viewRoles()
+      break;
+
+      case "view employees":
+      viewEmployees()
+      break;
+
+      case "update employee role":
+      updateEmployeeRole()
+      break;
+  }
+})};
+
+
+// function to be called for "add department as a choice"
+
+function addDepartment(){
+  console.log("department added");
+  // inquirer.prompt([
+  //   {
+  //     type : "input",
+  //     message : "Input the name of department you would like to add?",
+  //     name : "departmentName"
+  //   }
+  // ]).then(res => {
+  //   console.log("inserting new department")
+  //   connection.query("INSERT INTO department SET ?",{department : res.departmentName},
+  //   function(err, res) {
+  //     if (err) throw err;
+  //     startApp();
+      
+  //   }
+  //   )
+  // })
+}
--- employeetracker.sql
+++ employeetracker.sql
@@ -1,24 +1,27 @@
 DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
-USE employee_tracker;\
+USE employee_tracker;
 
 
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(30)
-)
+    id INT AUTO_INCREMENT NOT NULL ,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL  ,
     title VARCHAR(30),
     slary Decimal(10,2),
-    department_id INT NOT NULL
-)
+    department_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
-    role_id INT NOT NULL
-)
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
